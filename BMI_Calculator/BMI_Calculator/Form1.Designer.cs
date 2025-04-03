63555b365ac52461b2e1cf729c4c4b6610bebaac
Shavana <127197288+ShavanaM@users.noreply.github.com>
2025-04-03 12:43:01 +0200

    Add files via upload

---
+++ BMI_Calculator/BMI_Calculator/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BMI_Calculator
+{
+    partial class frm_BMI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBMICalculater = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBMICalculater
+            // 
+            this.btnBMICalculater.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBMICalculater.Location = new System.Drawing.Point(115, 344);
+            this.btnBMICalculater.Name = "btnBMICalculater";
+            this.btnBMICalculater.Size = new System.Drawing.Size(172, 42);
+            this.btnBMICalculater.TabIndex = 0;
+            this.btnBMICalculater.Text = "BMI Calculator";
+            this.btnBMICalculater.UseVisualStyleBackColor = true;
+            this.btnBMICalculater.Click += new System.EventHandler(this.btnBMICalculater_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter your Weight in Kilograms";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(219, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter your Height in Meters";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(115, 237);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(172, 86);
+            this.lblDisplay.TabIndex = 3;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWeight.Location = new System.Drawing.Point(309, 73);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(100, 24);
+            this.txtWeight.TabIndex = 4;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHeight.Location = new System.Drawing.Point(309, 153);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 24);
+            this.txtHeight.TabIndex = 5;
+            // 
+            // frm_BMI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 414);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBMICalculater);
+            this.Name = "frm_BMI";
+            this.Text = "BMI Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBMICalculater;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtHeight;
+    }
+}
+
---
+++ BMI_Calculator/BMI_Calculator/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_Calculator
+{
+    public partial class frm_BMI : Form
+    {
+        public frm_BMI()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBMICalculater_Click(object sender, EventArgs e)
+        {
+            double BMI, Weight, Height;
+
+            Weight = 0;
+            Height = 0;
+            BMI = 0;
+
+            Weight = Convert.ToDouble(txtWeight.Text);
+            Height = Convert.ToDouble(txtHeight.Text);
+
+            BMI = Weight / (Height * Height);
+
+            lblDisplay.Text = "Your BMI is : "+ BMI; 
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
